ce90478fac6d43184dce30f2e92fe9a8faa1459d
Niel Hekkens <nielhekkens@gmail.com>
2019-04-26 23:00:27 +0200

    Style registration page

--- src/views/register/RegisterView.vue
+++ src/views/register/RegisterView.vue
@@ -1,13 +1,34 @@
 <template>
-	<div class="RegisterView">
-		<h1>Register now</h1>
-		<input v-model="email" type="text" name="email" id="Email" placeholder="Email">
-		<input v-model="password" type="password" name="password" id="Password" placeholder="Password">
-		
-		<button @click="onRegistrationClicked()">Create account</button>
-
-		<p>Already have an account yet? <router-link to="/login">Login here</router-link>.</p>
+<div class="RegisterView">
+	<div class="HeroContainer">
+		<form class="Form RegistrationForm">
+			<h1>Team manager registration</h1>
+			<div class="FormInputContainer">
+				<section>
+					<label for="Email">Email</label>
+					<input
+						v-model="email"
+						type="text"
+						name="email"
+						id="Email">
+				</section>
+				<section>
+					<label for="Password">Password</label>
+					<input
+						v-model="password"
+						type="password"
+						name="password"
+						id="Password">
+					<p class="GoToLogin">Already have an account? <router-link to="/login">Login here</router-link>.</p>
+				</section>
+			</div>
+			<button 
+				@click="onRegistrationClicked()"
+				class="Button SubmitButton">Create account</button>
+		</form>
 	</div>
+</div>
 </template>
 
 <script lang="ts" src="./RegisterView.ts"></script>
+<style lang="scss" scoped src="./RegisterView.scss"></style>
