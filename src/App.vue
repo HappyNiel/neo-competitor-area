2bffb9ac1d501649b6e24f5ab0ad7e663aa64325
Niel Hekkens <nielhekkens@gmail.com>
2019-06-18 20:24:51 +0200

    Add a footer to the project.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
 	<div id='app'>
-		<the-header></the-header>
-		<router-view/>
+        <div class="content">
+            <the-header></the-header>
+            <router-view/>
+        </div>
+        <the-footer></the-footer>
 	</div>
 </template>
 
 <script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator';
 import TheHeader from '@/components/the-header/the-header';
+import TheFooter from '@/components/the-footer/the-footer';
 
 @Component({
     components: {
-        TheHeader
+        TheHeader,
+        TheFooter
     }
 })
 export default class App extends Vue {
 
 }
 </script>
-
-
 <style lang='scss' src='./styles/main.scss'></style>
