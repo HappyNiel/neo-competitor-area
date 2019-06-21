2471f365ed762fed8e3c854432d2efd44c938629
Niel Hekkens <nielhekkens@gmail.com>
2019-06-21 09:26:06 +0200

    The profile data now gets loaded on refresh

--- src/views/profile/ProfileView.ts
+++ src/views/profile/ProfileView.ts
@@ -1,4 +1,4 @@
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component, Watch } from 'vue-property-decorator';
 import { globalStore } from '@/store';
 import { UserProfile } from '@/infrastructure/interfaces/user-profile.interface';
 import FirestoreAPI from '@/infrastructure/api/firestore.api';
@@ -6,17 +6,15 @@ import authService from '@/infrastructure/services/authentication.service';
 
 @Component
 export default class ProfileView extends Vue {
-    public userProfile: UserProfile = {firstName: 'this', lastName: 'name'};
-    public email: string = 'email@address.com';
     private fireStore: FirestoreAPI = new FirestoreAPI();
 
-    public mounted() {
-        this.fillUserInfo();
+    public get userProfile(): UserProfile {
+        return globalStore.user.profile;
     }
 
-    // TODO: Figure out why the data is not loading on initial page load.
-    // Probably something with the async loading of the data and the component
-    // is not updated when the data is updated for the first time.
+    public get email(): string {
+        return globalStore.user.email;
+    }
 
     public onUpdateProfileClicked(): void {
         const userId = globalStore.user.uid;
@@ -27,18 +25,5 @@ export default class ProfileView extends Vue {
 
     public onResetPasswordClicked(): void {
         authService.resetPassword(this.email);
-    }
-
-    private fillUserInfo(): void {
-        this.getEmailAddress();
-        this.getUserProfile();
-    }
-
-    private getEmailAddress(): void {
-        this.email = globalStore.user.email;
-    }
-
-    private getUserProfile(): void {
-        this.userProfile = globalStore.user.profile;
     }
 }
