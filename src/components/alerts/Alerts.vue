4c5ab0e3513e0d440328f495f5b0ed533c101884
Niel Hekkens <nielhekkens@gmail.com>
2019-11-21 16:44:31 +0100

    Make alert dismissable

--- src/components/alerts/Alerts.vue
+++ src/components/alerts/Alerts.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="AlertsContainer">
         <b-alert
-            :show="eventBus.isSuccess">
+            :show="eventBus.isSuccess"
+            dismissible>
             Success!
         </b-alert>
     </div>
